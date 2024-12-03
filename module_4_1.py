da387444aa01c6247dd64e1b8d3a756bdc88b8c9
Repik777 <vitya.mylnikov.03@gmail.com>
2024-12-03 14:10:17 +0400

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+import fake_math as fm
+import true_math as tm
+
+result1 = fm.divide1(69, 3)
+result2 = fm.divide1(3, 0)
+result3 = tm.divide2(49, 7)
+result4 = tm.divide2(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
